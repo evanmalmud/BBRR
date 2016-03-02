670dff6cf2d22c21716855d0af0e856b9c93da30
Dave McHale <dmchale@gmail.com>
2016-03-02 13:25:14 -0500

    refactored team logic into function that takes Home/Away to query proper table, just wanted a way to quickly look at both teams

--- index.php
+++ index.php
@@ -2,21 +2,39 @@
 require_once("classes/global_class_include.php");
 
 $myDB = new SQLiteDB("replays/Replay_2016-02-24_21-12-09.db");
-
 if ( isset( $myDB->error ) )
     die( $myDB->error );
 
-$query = "SELECT ID, strName, idRaces FROM Away_Player_Listing";
-$result = $myDB->query($query);
+showTeamData( "Home" );
+showTeamData( "Away" );
 
-if ( ! $result ) {
-    echo "Empty result";
-} else {
+/******************************************************************************************
+ ******************************************************************************************
+ ******************************************************************************************
+ ******************************************************************************************
+ */
 
-    $result_array = $result->fetchAll();
-    $teamRace = BBRRTeam::get_team_race( $result_array[0]["idRaces"] );
+/**
+ * @param string $teamType
+ */
+function showTeamData( $teamType = "Home" )
+{
+    global $myDB;
 
-    foreach ($result_array as $row) {
-        echo "<strong>" . $teamRace . "</strong>: player " . $row["ID"] . ": " . $row["strName"] . "<br>";
+    echo "<h3>" . $teamType . " team</h3>";
+
+    $query = "SELECT ID, strName, idRaces FROM " . $teamType . "_Player_Listing";
+    $result = $myDB->query($query);
+
+    if (!$result) {
+        echo "Empty result";
+    } else {
+
+        $result_array = $result->fetchAll();
+        $teamRace = BBRRTeam::get_team_race($result_array[0]["idRaces"]);
+
+        foreach ($result_array as $row) {
+            echo "<strong>" . $teamRace . "</strong>: player " . $row["ID"] . ": " . $row["strName"] . "<br>";
+        }
     }
 }
