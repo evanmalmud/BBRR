807d58d9747c388421e289a2ee04792ed8e4eb86
Dave McHale <dmchale@gmail.com>
2016-03-09 12:57:29 -0500

    Updating function calls to try and reduce complexity for CodeClimate results.

--- blockDiceStats.php
+++ blockDiceStats.php
@@ -24,17 +24,26 @@ if ( isset( $_REQUEST["logFile"] ) ) {
  */
 function showBlockDice()
 {
-    global $arrLog;
     global $arrTeams;
-
-    $lastLine = "";
-    $foundTeams = false;
 
     echo "<h3>Block Dice</h3>";
 
+    putDiceStatsInTeamObject();
+
+    printDiceStats();
+
+}
+
+function putDiceStatsInTeamObject()
+{
+    global $arrLog;
+    $lastLine = "";
+
+    $foundTeams = false;
+
     foreach ($arrLog as $line) {
 
-        if ( ( ! $foundTeams ) && isTeamVsTeamDeclaration( $line ) ) {
+        if ((!$foundTeams) && isTeamVsTeamDeclaration($line)) {
             $team1 = getTeam1FromTeamDeclaration($line);
             $team2 = getTeam2FromTeamDeclaration($line);
 
@@ -42,52 +51,48 @@ function showBlockDice()
 
             $foundTeams = true;
 
-        } elseif ( isBlockResult( $line ) ) {
+        } elseif (isBlockResult($line)) {
             trackBlockDice($line, $lastLine);
         }
         $lastLine = $line;
     }
+}
+
+function printDiceStats()
+{
+    global $arrTeams;
 
     // Now display the block dice data from the teams array
     foreach ($arrTeams as $team) {
 
-        $teamName                   = $team["Name"];
+        $teamName = $team["Name"];
 
-        $totalDice                  = $team["attacker down"] + $team["both down"] + $team["pushed"] + $team["defender stumbles"] + $team["defender down"];
+        $totalDice = $team["attacker down"] + $team["both down"] + $team["pushed"] + $team["defender stumbles"] + $team["defender down"];
 
-        $attackerDown               = $team["attacker down"];
-        $bothDown                   = $team["both down"];
-        $pushed                     = $team["pushed"];
-        $defenderStumbles           = $team["defender stumbles"];
-        $defenderDown               = $team["defender down"];
-        $doubleSkulls               = $team["double skulls"];
-        $doublePows                 = $team["double pows"];
+        $attackerDownAvg = round($totalDice / 6, 2);
+        $bothDownAvg = round($totalDice / 6, 2);
+        $pushedAvg = round($totalDice / 3, 2);              // Remember there are 2x as many chances to roll a Push
+        $defenderStumblesAvg = round($totalDice / 6, 2);
+        $defenderDownAvg = round($totalDice / 6, 2);
 
-        $attackerDownAverage        = round( $totalDice / 6, 2 );
-        $bothDownAverage            = round( $totalDice / 6, 2 );
-        $pushedAverage              = round( $totalDice / 3, 2 );              // Remember there are 2x as many chances to roll a Push
-        $defenderStumblesAverage    = round( $totalDice / 6, 2 );
-        $defenderDownAverage        = round( $totalDice / 6, 2 );
-
-        $attackerClass              = $attackerDown >= $attackerDownAverage ? "green" : "red";
-        $bothDownClass              = $bothDown >= $bothDownAverage ? "green" : "red";
-        $pushedClass                = $pushed >= $pushedAverage ? "green" : "red";
-        $defenderStumblesClass      = $defenderStumbles >= $defenderStumblesAverage ? "green" : "red";
-        $defenderDownClass          = $defenderDown >= $defenderDownAverage ? "green" : "red";
-        $doubleSkullsClass          = $doubleSkulls > 0 ? "red" : "green";
-        $doublePowsClass            = $doublePows > 0 ? "green" : "black";
+        $attackerClass = $team["attacker down"] >= $attackerDownAvg ? "green" : "red";
+        $bothDownClass = $team["both down"] >= $bothDownAvg ? "green" : "red";
+        $pushedClass = $team["pushed"] >= $pushedAvg ? "green" : "red";
+        $defenderStumblesClass = $team["defender stumbles"] >= $defenderStumblesAvg ? "green" : "red";
+        $defenderDownClass = $team["defender down"] >= $defenderDownAvg ? "green" : "red";
+        $doubleSkullsClass = $team["double skulls"] > 0 ? "red" : "green";
+        $doublePowsClass = $team["double pows"] > 0 ? "green" : "black";
 
         echo "<h3>" . $teamName . "</h3>";
         echo "<ul>";
-        echo "<li style=\"color:" . $attackerClass . "\">Attacker Down: " . $attackerDown . " (avg " . $attackerDownAverage . ")</li>";
-        echo "<li style=\"color:" . $bothDownClass . "\">Both Down: " . $bothDown . " (avg " . $bothDownAverage . ")</li>";
-        echo "<li style=\"color:" . $pushedClass . "\">Pushed: " . $pushed . " (avg " . $pushedAverage . ")</li>";
-        echo "<li style=\"color:" . $defenderStumblesClass . "\">Defender Stumbles: " . $defenderStumbles . " (avg " . $defenderStumblesAverage . ")</li>";
-        echo "<li style=\"color:" . $defenderDownClass . "\">Defender Down: " . $defenderDown . " (avg " . $defenderDownAverage . ")</li>";
-        echo "<li style=\"color:" . $doubleSkullsClass . "\">Double Skulls: " . $doubleSkulls . "</li>";
-        echo "<li style=\"color:" . $doublePowsClass . "\">Double Pows: " . $doublePows . "</li>";
+        echo "<li style=\"color:" . $attackerClass . "\">Attacker Down: " . $team["attacker down"] . " (avg " . $attackerDownAvg . ")</li>";
+        echo "<li style=\"color:" . $bothDownClass . "\">Both Down: " . $team["both down"] . " (avg " . $bothDownAvg . ")</li>";
+        echo "<li style=\"color:" . $pushedClass . "\">Pushed: " . $team["pushed"] . " (avg " . $pushedAvg . ")</li>";
+        echo "<li style=\"color:" . $defenderStumblesClass . "\">Defender Stumbles: " . $team["defender stumbles"] . " (avg " . $defenderStumblesAvg . ")</li>";
+        echo "<li style=\"color:" . $defenderDownClass . "\">Defender Down: " . $team["defender down"] . " (avg " . $defenderDownAvg . ")</li>";
+        echo "<li style=\"color:" . $doubleSkullsClass . "\">Double Skulls: " . $team["double skulls"] . "</li>";
+        echo "<li style=\"color:" . $doublePowsClass . "\">Double Pows: " . $team["double pows"] . "</li>";
         echo "<li>TOTAL DICE: " . $totalDice . "</li>";
         echo "</ul>";
     }
-
 }
